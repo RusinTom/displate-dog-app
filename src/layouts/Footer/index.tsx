5071f2ebdacf77f61ff47cff397b3536e1d67a14
RusinTom <tomaszrusin00@gmail.com>
2022-02-13 16:34:53 +0100

    style: add Footer styles

--- src/layouts/Footer/index.tsx
+++ src/layouts/Footer/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import { colors } from '@/styles/shared/colors'
 
 interface FooterProps {
   msg: string
 }
 
+const MainFooter = styled.footer`
+  background-color: ${colors.primaryColor};
+  padding: 0.5rem 1.5rem;
+`
+
+const FooterMessage = styled.p`
+  color: white;
+`
+
 const Footer = ({ msg }: FooterProps) => {
-  return <footer>{msg}</footer>
+  return (
+    <MainFooter>
+      <FooterMessage>{msg}</FooterMessage>
+    </MainFooter>
+  )
 }
 
 export default Footer
