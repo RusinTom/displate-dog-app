a92679abf571c46b0eccbb31a1ebaf5c62a0504d
RusinTom <tomaszrusin00@gmail.com>
2022-02-13 16:12:01 +0100

    style: add common styles for FlexWrapper

---
+++ src/components/common/FlexWrapper/index.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+interface FlexWrapperProps {
+  justifyContent?:
+    | 'space-between'
+    | 'space-around'
+    | 'space-evenly'
+    | 'center'
+    | 'start'
+    | 'end'
+    | 'flex-start'
+    | 'flex-end'
+  alignItems?: 'center' | 'stretch' | 'start' | 'end' | 'inherit'
+  wrap?: 'nowrap' | 'wrap' | 'wrap-reverse'
+}
+
+const FlexWrapper = styled.div<FlexWrapperProps>`
+  display: flex;
+  flex-wrap: ${props => props.wrap || 'nowrap'};
+  align-items: ${props => props.alignItems || 'inherit'};
+  justify-content: ${props => props.justifyContent || 'start'};
+`
+
+export default FlexWrapper
