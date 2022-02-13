ca8a2b1855b79d565775e1542aed917e1ac258b6
RusinTom <tomaszrusin00@gmail.com>
2022-02-13 16:35:40 +0100

    style: add Main content and AppContainer styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
+import styled from 'styled-components'
+
+import FlexWrapper from '@/components/common/FlexWrapper'
 import HelloWorld from '@/components/HelloWorld/HelloWorld'
 import Footer from '@/layouts/Footer'
 import Header from '@/layouts/Header'
 import GlobalStyle from '@/styles/GlobalStyle'
 
+const AppContainer = styled(FlexWrapper)`
+  min-height: 100vh;
+`
+
+const Main = styled.main`
+  flex: 1;
+`
+
 export default function App() {
   return (
     <>
       <GlobalStyle />
-      <Header title={'Dog App'} />
-      <main>
-        <HelloWorld msg="Boilerplate" />
-      </main>
-      <Footer msg={'© 2022 All Rights Reserved'} />
+      <AppContainer flexDirection="column" justifyContent="space-between">
+        <Header title={'Dog App'} />
+        <Main>
+          <HelloWorld msg="Boilerplate" />
+        </Main>
+        <Footer msg={'© 2022 All Rights Reserved'} />
+      </AppContainer>
     </>
   )
 }
