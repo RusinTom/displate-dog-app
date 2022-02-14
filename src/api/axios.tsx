86a346d49c4b5f7c9740639439c1b3cf28a5fbf8
RusinTom <tomaszrusin00@gmail.com>
2022-02-14 18:56:39 +0100

    refactor: refactor axios config
    Added interceptor that calls axiosErrorHandler method. It distinguishes the type of error.

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -1,10 +1,38 @@
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 
-const baseURL = 'https://dog.ceo/api/'
+enum ERROR_MESSAGES_ENUM {
+  apiError = 'Technical problems have occurred, please try again later.',
+  networkError = 'There were network problems. Check your internet connection and try again.',
+  syntaxError = 'Sorry, the download failed. We will try to fix the technical problems as soon as possible.'
+}
+
+const baseURL = 'https://dog.ceo/api'
 
 const apiClient = axios.create({
   baseURL // <- Could be ENV variable
 })
+
+const axiosErrorHandler = (error: AxiosError) => {
+  if (error.isAxiosError) {
+    if (error.response) {
+      console.error(`[API Error]: ${error.response.data}`)
+      return Promise.reject(ERROR_MESSAGES_ENUM.apiError)
+    } else {
+      console.error('[Network Error]: No Response Received')
+      return Promise.reject(ERROR_MESSAGES_ENUM.networkError)
+    }
+  } else {
+    console.error('[Non-HTTP Error]:', error)
+    return Promise.reject(ERROR_MESSAGES_ENUM.syntaxError)
+  }
+}
+
+apiClient.interceptors.response.use(
+  response => response,
+  async (error: AxiosError) => {
+    return axiosErrorHandler(error)
+  }
+)
 
 const { get, post, put, delete: destroy } = apiClient
 
