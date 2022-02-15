35b26850cb2b9dcc316116a076cb1373d6c0e38b
RusinTom <tomaszrusin00@gmail.com>
2022-02-15 21:09:36 +0100

    refactor: refactor DogBreeds api endpoints

--- src/api/Dogs/DogBreeds.tsx
+++ src/api/Dogs/DogBreeds.tsx
@@ -1,20 +1,21 @@
 import { AxiosResponse } from 'axios'
 
 import { DOGS_API_ENDPOINTS_ENUM } from '@/api/Dogs/index'
-import { DogBreedInterface } from '@/types/interfaces/DogBreedInterface'
+import { IDogBreed } from '@/types/interfaces/IDogBreed'
+import { IDogBreedImage } from '@/types/interfaces/IDogBreedImage'
 
 import { get } from '../axios'
 
 const createSingleBreedEndpoint = (breed: string): string => {
-  return `/breeds/${breed}/images/random`
+  return `/breed/${breed}/images/random`
 }
 
 interface DogBreedsApi {
-  index: () => Promise<AxiosResponse<DogBreedInterface>>
-  single: (breed: string) => Promise<AxiosResponse>
+  index: () => Promise<AxiosResponse<IDogBreed>>
+  single: (breed: string) => Promise<AxiosResponse<IDogBreedImage>>
 }
 
 export const DogBreeds: DogBreedsApi = {
-  index: () => get<DogBreedInterface>(DOGS_API_ENDPOINTS_ENUM.allBreeds),
-  single: breed => get(createSingleBreedEndpoint(breed))
+  index: () => get<IDogBreed>(DOGS_API_ENDPOINTS_ENUM.allBreeds),
+  single: breed => get<IDogBreedImage>(createSingleBreedEndpoint(breed))
 }
