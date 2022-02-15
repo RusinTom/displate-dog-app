2b9e87fea15dd076b60bd4384c6579f53644172d
RusinTom <tomaszrusin00@gmail.com>
2022-02-15 21:07:03 +0100

    style: move styles from DogList and LoadingSpinner to seperate files

---
+++ src/components/Dog/DogList/styles/DogListStyles.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+import { FlexWrapper } from '@/components/common/FlexWrapper'
+import { breakpoints, maxWidth } from '@/styles/shared/breakpoints'
+import { colors } from '@/styles/shared/colors'
+
+export const Card = styled.div`
+  flex: 1 1 calc(33.33% - 3rem);
+  margin: 1.5rem;
+  padding: 1.5rem;
+  border-radius: 4px;
+  box-shadow: 0 16px 32px rgb(180 180 180 / 40%);
+  background-color: ${colors.gray};
+
+  ${maxWidth(breakpoints.lg)} {
+    flex: 1 1 calc(50% - 3rem);
+  }
+
+  ${maxWidth(breakpoints.sm)} {
+    flex: 1 1 100%;
+  }
+`
+
+export const DogListWrap = styled(FlexWrapper)`
+  margin: 0 -1.5rem;
+`
+
+export const ButtonsWrap = styled.div`
+  margin: 0 -0.5rem;
+`
+
+export const Title = styled.h3`
+  margin-bottom: 1rem;
+  text-transform: capitalize;
+`
---
+++ src/components/common/LoadingSpinner/styles/LoadingSpinnerStyles.tsx
@@ -0,0 +1,58 @@
+import styled, { keyframes } from 'styled-components'
+
+import { colors } from '@/styles/shared/colors'
+
+interface RingProps {
+  size: number
+}
+
+interface RingElementProps {
+  size: number
+  borderSize: number
+  borderColor?: string
+}
+
+const animate = keyframes`
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+`
+
+export const Ring = styled.div<RingProps>`
+  display: inline-block;
+  position: relative;
+  width: ${props => props.size + 'px'};
+  height: ${props => props.size + 'px'};
+  margin-left: 2rem;
+`
+
+export const RingElement = styled.div<RingElementProps>`
+  display: block;
+  position: absolute;
+  width: ${props => props.size + 'px'};
+  height: ${props => props.size + 'px'};
+  border: ${props =>
+    props.borderColor
+      ? `${props.borderSize}px solid ${props.borderColor}`
+      : `${props.borderSize}px solid ${colors.turquoise}`};
+  border-radius: 50%;
+  animation: ${animate} 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: ${props =>
+    props.borderColor
+      ? `${props.borderColor} transparent transparent transparent;`
+      : `${colors.turquoise} transparent transparent transparent`};
+`
+
+export const RingElementSecond = styled(RingElement)`
+  animation-delay: -0.45s;
+`
+export const RingElementThird = styled(RingElement)`
+  animation-delay: -0.3s;
+`
+
+export const RingElementFourth = styled(RingElement)`
+  animation-delay: -0.15s;
+`
